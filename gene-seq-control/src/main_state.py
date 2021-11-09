a0950155180ddac5ccb8bd23db623929f054a863
cyanial <chenjie3323@sjtu.edu.cn>
2021-11-09 20:57:48 +0800

    singleton state class

---
+++ gene-seq-control/src/main_state.py
@@ -0,0 +1,17 @@
+"""
+Singleton Object represent platform state
+
+
+2021/11/09
+"""
+
+class main_state():
+    _instance = None
+
+    def __new__(cls, *args, **kw):
+        if cls._instance is None:
+            cls._instance = object.__new__(cls, *args, **kw)
+        return cls._instance
+
+    def __init__(self):
+        pass
